e31977cb5ea0f9dc079ba763a0555fd992997350
azertypow <nico@villa1203.ch>
2026-09-15 21:02:15 +0200

    feat: init hello test

---
+++ app/components/Hello.global.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="v-hello"
+    >
+      <p>{{ props.message }}</p>
+      <p>{{ props.color }}</p>
+    </section>
+</template>
+
+
+
+
+
+<script setup lang="ts">
+const props = defineProps<{
+  message?: string,
+  color?: string,
+}>()
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-hello {
+}
+</style>
