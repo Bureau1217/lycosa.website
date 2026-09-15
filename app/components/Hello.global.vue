9ec15593e62681a0c764073016053234cdfc1aee
azertypow <nico@villa1203.ch>
2026-09-15 21:56:02 +0200

    feat: teste props nuxtStudio

--- app/components/Hello.global.vue
+++ app/components/Hello.global.vue
@@ -1,14 +1,10 @@
 <template>
-    <section class="v-hello"
+    <section class="v-hello" :style="{ color: props.color }"
     >
       <p>{{ props.message }}</p>
       <p>{{ props.color }}</p>
     </section>
 </template>
-
-
-
-
 
 <script setup lang="ts">
 const props = defineProps<{
@@ -16,10 +12,6 @@ const props = defineProps<{
   color?: string,
 }>()
 </script>
-
-
-
-
 
 <style lang="scss" scoped >
 .v-hello {
