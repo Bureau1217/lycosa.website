44067c91141aaafef86c6837a8d015d0e2d2b278
azertypow <nico@villa1203.ch>
2026-09-16 16:01:30 +0200

    feat: new tile texte block

---
+++ app/components/BlockTextTile.global.vue
@@ -0,0 +1,44 @@
+<template>
+    <section class="v-block-text_tile"
+    >
+      <div class="v-block-text_tile__top v-remove-last-and-first-child-margin">
+        <slot name="top"/>
+      </div>
+
+      <div class="v-block-text_tile__bottom v-remove-last-and-first-child-margin">
+        <slot name="bottom"/>
+      </div>
+
+      <div class="v-block-text_tile__index">
+        {{index}}
+      </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+
+defineProps<{
+  index: number
+}>()
+
+</script>
+
+<style lang="scss" scoped >
+.v-block-text_tile {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: solid 1px var(--v-color-black);
+  gap: 10rem;
+  padding: var(--v-gutter);
+}
+
+.v-block-text_tile__index {
+  position: absolute;
+  top:    var(--v-gutter);
+  right:  var(--v-gutter);
+}
+</style>
