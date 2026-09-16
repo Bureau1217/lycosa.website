6939bd0e67de4b98f0286e22b42fc2d2e3c5062f
azertypow <nico@villa1203.ch>
2026-09-16 14:02:19 +0200

    feat: init newsletter text and typo params

---
+++ app/components/BlockNewsletter.global.vue
@@ -0,0 +1,59 @@
+<template>
+    <section class="v-block-newsletter"
+    >
+      <div class="v-block-newsletter__content">
+        <h5 class="v-block-newsletter__title"
+        >{{ title || 'Vous voulez être au courant de la suite ?' }}</h5>
+
+        <form class="v-block-newsletter__form v-form"
+              @submit.prevent="onSubmit"
+        >
+          <input class="v-block-newsletter__input"
+                 type="email"
+                 v-model="email"
+                 :placeholder="placeholder || 'Votre email'"
+                 required
+          />
+          <button class="v-block-newsletter__submit"
+                  type="submit"
+                  aria-label="S'abonner"
+          >&rarr;</button>
+        </form>
+      </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  title?: string
+  placeholder?: string
+}>()
+
+const emit = defineEmits<{
+  subscribe: [email: string]
+}>()
+
+const email = ref('')
+
+function onSubmit() {
+  emit('subscribe', email.value)
+  email.value = ''
+}
+</script>
+
+<style lang="scss" scoped >
+.v-block-newsletter {
+  width: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  padding: var(--v-gutter--half);
+}
+
+.v-block-newsletter__content {
+  text-align: right;
+  background: lightgray;
+}
+</style>
