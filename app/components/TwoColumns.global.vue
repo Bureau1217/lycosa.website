6e104da21b289256b155a7de9c77afa6707a11fe
azertypow <nico@villa1203.ch>
2026-09-16 13:27:58 +0200

    feat: add coll section

---
+++ app/components/TwoColumns.global.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="v-two-columns"
+    >
+      <div class="v-two-columns__col">
+        <slot name="left" />
+      </div>
+      <div class="v-two-columns__col">
+        <slot name="right" />
+      </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+defineSlots<{
+  left?: () => any
+  right?: () => any
+}>()
+</script>
+
+<style lang="scss" scoped >
+.v-two-columns {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: var(--v-gutter, 2rem);
+}
+
+.v-two-columns__col {
+  min-width: 0;
+}
+</style>
