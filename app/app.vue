91b2dffe418a365d0a3b0e226c1eac4381577943
azertypow <nico@villa1203.ch>
2026-09-16 19:26:24 +0200

    feat: init footer

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,48 @@
+<script setup lang="ts">
+</script>
 <template>
   <div>
     <NuxtPage />
   </div>
+  <footer>
+    <TwoColumns
+    >
+      <template #left >
+        <h2>REJOINDRE L’AVENTURE ?</h2>
+        <BlockNewsletter/>
+      </template>
+
+      <template #right >
+        <block-collumns>
+          <template #first >
+            <h5>Navigation</h5>
+            <div>
+              <a href="#">Vision</a>
+              <br/><a href="#">Distinction</a>
+              <br/><a href="#">Nos montres</a>
+              <br/><a href="#">Engagements</a>
+              <br/><a href="#">Presse</a>
+            </div>
+          </template>
+          <template #second >
+            <h5>Navigation</h5>
+            <a href="#">Distinction</a>
+            <br/><a href="#">Distinction</a>
+            <br/><a href="#">Nos montres</a>
+            <br/><a href="#">Engagements</a>
+            <br/><a href="#">Presse</a>
+          </template>
+          <template #three >
+            <h5>Navigation</h5>
+            <a href="#">Distinction</a>
+            <br/><a href="#">Distinction</a>
+            <br/><a href="#">Nos montres</a>
+            <br/><a href="#">Engagements</a>
+            <br/><a href="#">Presse</a>
+          </template>
+        </block-collumns>
+      </template>
+
+    </TwoColumns>
+  </footer>
 </template>
